ea28909ddda211c5a0571cf0292cf6796fe958e6
huccct <ucccth@gmail.com>
2025-05-16 22:09:25 +0100

    chore: add husky to optimize commit process

---
+++ commitlint.config.ts
@@ -0,0 +1,25 @@
+export default {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat', // 新功能
+        'fix', // 修复
+        'docs', // 文档变更
+        'style', // 代码格式
+        'refactor', // 重构
+        'perf', // 性能优化
+        'test', // 增加测试
+        'build', // 构建系统
+        'ci', // CI 配置
+        'chore', // 其他改动
+        'revert', // 回退
+        'wip', // 开发中
+        'types', // 类型修改
+        'release', // 发布
+      ],
+    ],
+  },
+}
