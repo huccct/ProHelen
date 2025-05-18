03e3e2f92c2f45f60fa63f80165484e036bf2768
huccct <ucccth@gmail.com>
2025-05-18 13:08:56 +0100

    feat: add middleware to solve redirect

---
+++ middleware.ts
@@ -0,0 +1,37 @@
+import type { NextRequest } from 'next/server'
+import { getToken } from 'next-auth/jwt'
+import { NextResponse } from 'next/server'
+
+export async function middleware(request: NextRequest) {
+  const token = await getToken({ req: request })
+  const isAuthPage = request.nextUrl.pathname === '/'
+    || request.nextUrl.pathname.startsWith('/sign-in')
+    || request.nextUrl.pathname.startsWith('/sign-up')
+    || request.nextUrl.pathname.startsWith('/forgot-password')
+    || request.nextUrl.pathname.startsWith('/reset-password')
+
+  if (!token && !isAuthPage) {
+    const signInUrl = new URL('/sign-in', request.url)
+    signInUrl.searchParams.set('callbackUrl', request.nextUrl.pathname)
+    return NextResponse.redirect(signInUrl)
+  }
+
+  if (token && isAuthPage && request.nextUrl.pathname !== '/') {
+    return NextResponse.redirect(new URL('/', request.url))
+  }
+
+  return NextResponse.next()
+}
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     */
+    '/((?!api|_next/static|_next/image|favicon.ico).*)',
+  ],
+}
